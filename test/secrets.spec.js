551b807c37381c491445988a932cb3ea6761fc9a
Thiago Lagden <lagden@gmail.com>
2024-01-05 17:54:10 -0300

    update packs and test

--- test/secrets.spec.js
+++ test/secrets.spec.js
@@ -1,30 +1,31 @@
 import path from 'node:path'
 import {fileURLToPath} from 'node:url'
-import test from 'ava'
+import {test} from 'node:test'
+import assert from 'node:assert'
 import readSecrets from '../src/secrets.js'
 
 const __dirname = fileURLToPath(new URL('.', import.meta.url))
 
-test('basic', async t => {
+test('basic', async () => {
 	const file = path.join(__dirname, 'helper', 'file.txt')
 	const data = await readSecrets(file)
-	t.is(data, 'foo\n')
+	assert.equal(data, 'foo\n')
 })
 
-test('url', async t => {
+test('url', async () => {
 	const file = new URL('helper/file.txt', import.meta.url)
 	const data = await readSecrets(file)
-	t.is(data, 'foo\n')
+	assert.equal(data, 'foo\n')
 })
 
-test('url invalid', async t => {
+test('url invalid', async () => {
 	const file = new URL('invalid_file', import.meta.url)
 	const data = await readSecrets(file)
-	t.is(data, file)
+	assert.equal(data, file)
 })
 
-test('invalid', async t => {
+test('invalid', async () => {
 	const file = 'invalid_file'
 	const data = await readSecrets(file)
-	t.is(data, file)
+	assert.equal(data, file)
 })
